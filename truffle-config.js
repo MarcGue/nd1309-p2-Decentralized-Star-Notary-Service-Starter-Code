a5f7516f03d66a0e5e986fd59988948e597f38e5
Marc Wissenbach <marc.wissenbach@cegeka.de>
2021-06-08 15:25:20 +0200

    changed truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -22,11 +22,11 @@
  *
  */
 
-// const HDWallet = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWallet = require('truffle-hdwallet-provider');
+const infuraKey = "fj4jll3k.....";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -72,6 +72,11 @@ module.exports = {
       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
+
+    rinkeby: {
+      provider: () => new HDWallet(mnemonic, 'https://rinkeby.infura.io/v3/9c6288cb418f48a48094bb33548dd4f5'),
+      network_id: 4
+    }
 
     // Useful for private networks
     // private: {
